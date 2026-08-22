0ec2b53fe160e1b702458f552223830c8b1095a0
roy <robert@666.support>
2026-08-22 19:48:16 +0200

    update header

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: milnicki <milnicki@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/22 15:15:32 by milnicki          #+#    #+#             */
-/*   Updated: 2026/08/22 15:15:34 by milnicki         ###   ########.fr       */
+/*   Updated: 2026/08/22 19:48:03 by green            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef FT_PRINTF_H
@@ -20,8 +20,7 @@
 # define FD 1
 
 int	ft_printf(const char *s, ...);
-int	ft_numlen(long n);
-int	ft_strlen(char *s);
+
 int	ft_hexlen(long n);
 int	ft_putchar(char c);
 int	ft_putstr(char *s);
